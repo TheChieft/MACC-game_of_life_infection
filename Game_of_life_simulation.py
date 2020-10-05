ebc9f9181a8c404d4ea9b4576c941669d1bd35ef
Andres Yañez <aandresfelipeyanezz@gmail.com>
2020-10-05 14:50:36 -0500

    Game_progress

--- Game_of_life_simulation.py
+++ Game_of_life_simulation.py
@@ -1,6 +1,16 @@
 import pygame, sys
+from pygame import quit
 
 pygame.init()
+
+##CONSTANTES
+
+#creacion de costantes utilizadas para los individuos/cell
+blanco=(255,255,255) #color blanco para celulas vivas
+rojo=(255,0,0) #color rojo para celulas infectadas
+# " " el color para celulas muertas sera el del fondo
+
+
 
 '''
 definir caracteristicas de la ventana
@@ -10,6 +20,8 @@ def windows():      #Creacion de ventana
     screen = pygame.display.set_mode([WIDTH,HEIGHT]) #definir el tamaño de la ventana
     bg_color=(0,0,0) #Color del fondo de la ventana (negro)
     pygame.display.set_caption("Game of life: Pandemic simulator") #titulo de la ventana
+
+
 
 '''
 Mantener abierta la ventana
@@ -21,5 +33,6 @@ def windowsrun():
             if event.type==QUIT:
                 pygame.quit()
                 sys.exit()
-
+        pygame.display.update() #actualizacion de ventana por nuevos sucesos
+        
 windowsrun()
